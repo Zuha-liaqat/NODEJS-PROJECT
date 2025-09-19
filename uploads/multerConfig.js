125f3beed9fa5c57a7a861e6c0fa3c9eafb56b19
Zuha-liaqat <zuhaliaqat996@gmail.com>
2025-09-20 02:39:16 +0500

    Changes

--- uploads/multerConfig.js
+++ uploads/multerConfig.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Storage config
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "uploads")); // uploads folder
+    cb(null, path.join(__dirname, "../uploads")); // uploads folder
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
